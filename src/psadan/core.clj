ec206163dd50424a96499981f2ff3200ca9212fc
Daniel Barlow <dan@telent.net>
2013-03-11 14:06:19 +0000

    s/hword-at/halfword-at/g

--- src/psadan/core.clj
+++ src/psadan/core.clj
@@ -42,7 +42,7 @@
           (bit-shift-left (nth buf (+ 2 offset)) 16)
           (bit-shift-left  (nth buf (+ 3 offset)) 24)))
 
-(defn hword-at [buf offset]
+(defn halfword-at [buf offset]
   (bit-or (nth buf (+ 0 offset))
           (bit-shift-left (nth buf (+ 1 offset)) 8) ))
 
@@ -58,10 +58,10 @@
 
 (defn parse-message-from-buf [buf socket message-type]
   (let [object-id (word-at buf 0)
-        bytes (hword-at buf 6)
-        opcode (hword-at buf 4)
+        bytes (halfword-at buf 6)
+        opcode (halfword-at buf 4)
         object (find-object socket object-id)
-        interface-def (:interface object) ; (nth the-protocol (:interface object))
+        interface-def (:interface object) 
         message-def (nth (get interface-def message-type) opcode)
         args (buffer-get-arguments buf 8 (map :type (:args message-def)))]
     {:object-id object-id :bytes bytes
@@ -82,7 +82,7 @@
   ([buf socket message-type]
      (parse-messages-from-buf buf socket message-type 0))
   ([buf socket message-type start]
-     (let [length-bytes (hword-at buf (+ 6 start))
+     (let [length-bytes (halfword-at buf (+ 6 start))
            end (+ length-bytes start)
            message (parse-message-from-buf (subvec buf start end)
                                            socket message-type)]
