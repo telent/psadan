41a2dec9804e7f381ca37c0953628027a39b9f25
Daniel Barlow <dan@telent.net>
2013-03-11 13:55:26 +0000

    split the xml parsing into its own file/namespace

--- src/psadan/core.clj
+++ src/psadan/core.clj
@@ -1,61 +1,8 @@
 (ns psadan.core
-  (:require [clojure.xml]
-            [clojure.data.zip :as dz]
-            [clojure.data.zip.xml :as x]
-            [clojure.walk]
-            [clojure.zip :as z]))
-
-(defn parse-message [i m]
-  (let [el (z/node m)
-        descr (x/xml1-> m :description)
-        args (map (fn [a]
-                    (let [a (:attrs (z/node a))]
-                      {:name (:name a)
-                       :type (keyword (:type a))
-                       :interface (keyword (:interface a))}))
-                  (x/xml-> m :arg))
-        ]
-    {:index i
-     :name (keyword (:name (:attrs el)))
-     :summary (and descr (:summary (:attrs (z/node descr))))
-     :args args
-     }))
-
-(defn parse-enum [e]
-  (let [name (keyword (:name (:attrs (z/node e))))
-        entries (map z/node (x/xml-> e :entry))]
-    {:name name
-     :summary (:summary (:attrs (z/node (x/xml1-> e :description))))
-     :entries (map :attrs entries)
-     }))
-
-(defn parse-interface [i n]
-  (let [el (z/node n)
-        requests (map-indexed parse-message (x/xml-> n :request))
-        events (map-indexed parse-message (x/xml-> n :event))
-        enums (map parse-enum (x/xml-> n :enum))]
-    {:index i
-     :name (keyword (:name (:attrs el)))
-     :requests requests
-     :events events
-     :enums enums
-     }))
-
-(defn parse-protocol [protocol]
-  (map-indexed parse-interface (x/xml-> protocol :interface)))
-
-(def the-protocol
-  (->
-   "/home/dan/wayland/source/wayland/protocol/wayland.xml"
-   clojure.xml/parse
-   z/xml-zip
-   parse-protocol))
-
-(defn find-interface-by-name [name]
-  (first (filter #(= (:name %) name) the-protocol)))
+  (:require [psadan.protocol :as proto]))
 
 (def global-object
-  {:interface (find-interface-by-name :wl_display)
+  {:interface (proto/find-interface-by-name :wl_display)
    :id 1})
 
 (defn open-socket [name]
