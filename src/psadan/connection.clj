e12f162d8e6451d33545899bed80266e516833ad
Daniel Barlow <dan@telent.net>
2013-03-11 22:49:29 +0000

    oops, looks like I forgot to commit this already

---
+++ src/psadan/connection.clj
@@ -0,0 +1,28 @@
+(ns psadan.connection
+  (:require [psadan.protocol :as proto]))
+
+(defn global-object-factory []
+  ;; this is a singleton object according to wayland, but will
+  ;; be a different singleton for each connection, iyswim
+  {:interface (proto/find-interface-by-name :wl_display)
+   :id 1})
+
+(defn open-connection [name]
+  (let [s (cx.ath.matthew.unix.UnixSocket. name)
+        in (. s getInputStream)
+        out (. s getOutputStream)
+        wl-display (global-object-factory)
+        ]
+    {:socket s
+     :input in
+     :output out
+     :display wl-display
+     :objects (atom (assoc {} 1 wl-display))
+     }))
+
+(defn remember-object [conn id object]
+  (swap! (:objects conn) assoc id object))
+
+(defn get-object [conn id]
+  (let [o (get @(:objects conn) id)]
+    o))
