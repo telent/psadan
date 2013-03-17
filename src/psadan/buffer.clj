4df9d2eac3cf561eee6d2d6d7812ff75c58abeaa
Daniel Barlow <dan@telent.net>
2013-03-17 10:51:04 +0000

    move buffer packing funcs into their own namespace where connection can require them

--- src/psadan/buffer.clj
+++ src/psadan/buffer.clj
@@ -34,30 +34,7 @@
                         ;; lose leading length word, trailing \0
                         (subvec buffer start (+ start -1 len))))))))
 
-(defn pack-arg [type value]
-  (case type
-    :new_id
-    (pack-arg :uint (:id value))
-    (:int :uint :object)
-    [(bit-shift-right (bit-and value 0x000000ff) 0)
-     (bit-shift-right (bit-and value 0x0000ff00) 8)
-     (bit-shift-right (bit-and value 0x00ff0000) 16)
-     (bit-shift-right (bit-and value 0xff000000) 24)]
-    :string
-    (let [bytes (.getBytes value)]
-      (vec (concat (pack-arg :int (count bytes))
-                   (vec bytes)
-                   (subvec [0 0 0 0] (mod (count bytes) 4)))))))
 
-(defn test-pack-arg []
-  (assert (= (map #(format "%x" %) (pack-arg :int 0x12345678))
-             '("78" "56" "34" "12")))
-  (assert (= (pack-arg :string "hello")
-             [5 0 0 0 104 101 108 108 111 0 0 0]))
-  (assert (= (pack-arg :new_id {:id 1234}) [210 4 0 0]))
-  (assert (= (count (pack-arg :string "hello ")) 12))
-  (assert (= (count (pack-arg :string "hello 1")) 12))
-  (assert (= (count (pack-arg :string "hello 12")) 16)))
 
 
 (defn parse-message [buf connection message-type]
@@ -85,18 +62,4 @@
          (conj '() message)
          (conj (parse-messages buf socket message-type end)
                message)))))
-
-
-(defn pack-message [connection object message-type message-name args]
-  (let [iface (:interface object)
-        messages (get iface message-type)
-        message (first (filter #(= (:name %) message-name) messages))
-        arg-types (map :type (:args message))
-        arg-bytes (mapcat pack-arg arg-types args)]
-    (vec (concat (pack-arg :uint (:id object))
-                 (pack-arg :uint
-                           (bit-or (bit-shift-left (+ 8 (count arg-bytes)) 16)
-                                   (:index message)))
-                 arg-bytes
-                 ))))
 
